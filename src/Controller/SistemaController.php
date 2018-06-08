235c205c6b20f33ead4b1b4ddb965a8c7067e9e9
rajmondik1 <rajmondik1@gmail.com>
2018-06-08 11:25:32 +0300

    text

--- src/Controller/SistemaController.php
+++ src/Controller/SistemaController.php
@@ -3,10 +3,18 @@
 namespace App\Controller;
 
 
+use App\Entity\Elektra;
+use App\Entity\SaltasVanduo;
+use App\Entity\Vanduo;
+use App\Form\ElektraType;
+use App\Form\SaltasVanduoType;
+use App\Form\VanduoType;
 use App\Repository\ElektraRepository;
 use App\Repository\SaltasVanduoRepository;
 use App\Repository\VanduoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SistemaController extends AbstractController
@@ -23,6 +31,51 @@ class SistemaController extends AbstractController
             'saltas_vanduos' => $saltasVanduoRepository->findBy(['userid' => $this->getUser()->getId()], ['date' => 'ASC']),
             'elektras' => $elektraRepository->findBy(['userid' => $this->getUser()->getId()], ['date' => 'ASC']),
         ]);
+        }
 
+    /**
+     * suds kazkoks , nes neveikia
+     */
+
+    /**
+     * @Route("/new", name="new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+
+        $vanduo = new Vanduo();
+        $vanduo->setUserid($this->getUser()->getId());
+        $form = $this->createForm(VanduoType::class, $vanduo);
+        $form->handleRequest($request);
+
+        $elektra = new Elektra();
+        $elektra->setUserid($this->getUser()->getId());
+        $formm = $this->createForm(ElektraType::class, $elektra);
+        $formm->handleRequest($request);
+
+        $saltasVanduo = new SaltasVanduo();
+        $saltasVanduo->setUserid($this->getUser()->getId());
+        $formmm = $this->createForm(SaltasVanduoType::class, $saltasVanduo);
+        $formmm->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($vanduo);
+            $em->persist($elektra);
+            $em->persist($saltasVanduo);
+            $em->flush();
+
+            return $this->redirectToRoute('sistema');
+        }
+
+        return $this->render('sistema/new.html.twig', [
+            'vanduo' => $vanduo,
+            'elektra' => $elektra,
+            'form' => $form->createView(),
+            'formm' => $formm->createView(),
+            'formmm' => $formmm->createView(),
+        ]);
     }
+
 }
